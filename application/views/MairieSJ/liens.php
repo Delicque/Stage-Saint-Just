4c674d38063aa18da4263c97618c64668da86d89
Delicque <44772637+Delicque@users.noreply.github.com>
2021-02-19 11:01:43 +0100

    faute ortho

--- application/views/MairieSJ/liens.php
+++ application/views/MairieSJ/liens.php
@@ -2,7 +2,7 @@
    
     <div id="intro" class="row"> 
         <div class="col-12">
-            <h3 class="d-flex justify-content-center"><FONT COLOR="black">Bienvenue à la Mairie de votre ville Saint Just en Chaussé</FONT></h3>
+            <h3 class="d-flex justify-content-center"><FONT COLOR="black">Bienvenue à la Mairie de votre ville Saint Just en Chaussée</FONT></h3>
         </div>
     </div>
     
